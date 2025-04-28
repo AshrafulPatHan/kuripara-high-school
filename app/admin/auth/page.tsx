43a9b0b5723b7349498e4e6e98395a44b9cd670d
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-28 13:47:33 +0600

    add app rout

---
+++ app/admin/auth/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function AdminAuth() {
+   return(
+      <div>
+         admin
+      </div>
+   )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,8 @@
 
 export default function Home(){
   return(
-    <div className="flex flex-col items-center">
-      <div className="fontSatisfy">
-        <p className="text-[36px] text-center ">hello 
-          Everyone has the right to rest and leisure, including reasonable limitation of working hours and periodic holidays.
-        </p>
-      </div>
-      <div style={{fontFamily: 'inter'}}>
-        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, recusandae.</h4>
-      </div>
+    <div>
+      hello
     </div>
   )
 }
