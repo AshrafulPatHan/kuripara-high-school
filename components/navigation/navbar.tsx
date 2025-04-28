2656788fd837447d08244c2643bf98540eb347c2
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-28 15:19:38 +0600

    add nav temp

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -10,6 +10,13 @@ export default function Navbar() {
                <p>Kuripara High School</p>
                <p>কুড়িপাড়া উচ্চ বিদ্যালয়</p>
             </div>
+            <div>
+               <p>ABOUT</p>
+               <p>NOTICES</p>
+               <p>EVENTS</p>
+               <p>NEWS</p>
+               <p>LINKS</p>
+            </div>
          </div>
       </div>
    )
