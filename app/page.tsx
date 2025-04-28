048321968c4c9ab56c8e8afef960ef52cc25403a
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-28 15:13:37 +0600

    add into page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
+import Events from "@/components/home/Events";
+import Hero from "@/components/home/hero";
+import History from "@/components/home/history";
+import News from "@/components/home/News";
+import Notices from "@/components/home/Notices";
+import Number from "@/components/home/number";
+import Teacher from "@/components/home/teacher";
+import Footer from "@/components/navigation/footer";
+import Navbar from "@/components/navigation/navbar";
 
 
 export default function Home(){
   return(
     <div>
-      hello
+      <Navbar/>
+      <Hero/>
+      <History/>
+      <Number/>
+      <Teacher/>
+      <News/>
+      <Notices/>
+      <Events/>
+      <Footer/>
     </div>
   )
 }
--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image";
 
 
 export default function Navbar() {
    return(
       <div>
-         navbar
+         <div>
+            <div>
+               <Image src="" alt="Logo" />
+               <p>Kuripara High School</p>
+               <p>কুড়িপাড়া উচ্চ বিদ্যালয়</p>
+            </div>
+         </div>
       </div>
    )
 }
