bd9822f94ff1bcdb9cacaad68b7c6bc2edda94d0
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-28 12:07:15 +0600

    lastly i add font suscesfully

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
 import Providers from "@/redux/Provider";
 import { Toaster } from "react-hot-toast";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Kuripaga High school",
@@ -33,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
-      >
+      <body>
         <Providers>
           <Theme>
             <Toaster position="top-center" />
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@
 
 export default function Home(){
   return(
-    <div>
-      hello world
+    <div className="flex flex-col items-center">
+      <div className="fontSatisfy">
+        <p className="text-[36px] text-center ">hello 
+          Everyone has the right to rest and leisure, including reasonable limitation of working hours and periodic holidays.
+        </p>
+      </div>
+      <div style={{fontFamily: 'Satisfy-Regular'}}>
+        <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea, recusandae.</h4>
+      </div>
     </div>
   )
 }
