3063f8247f3cec0baa2b8eaea86e47edd552809c
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-05-27 14:52:57 +0600

    add nav and hero section boilerplate

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
+import Hero_img from "@/assets/image/school/hero.jpg";
 
 
 export default function Hero() {
    return(
       <div>
-         hero
+         <div className="relative">
+            <div>
+               <Image src={Hero_img} alt="Logo" className="w-[100vw] h-[49vw] object-cover " />
+            </div>
+            <div className="absolute top-4 left-6">
+               <h1 className="text-4xl font-bold">Kuripara High School</h1>
+               <p className="text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo, blanditiis.</p>
+            </div>
+         </div>
       </div>
    )
 }
--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
+import Logo from "@/assets/image/gov.png";
 
 
 export default function Navbar() {
    return(
-      <div>
-         <div>
-            <div>
-               <Image src="" alt="Logo" />
+      <div className="bg-amber-200 py-2.5">
+         <div className="flex flex-row items-center justify-center gap-5">
+            <div  className="flex flex-row items-center">
+               <Image src={Logo} alt="Logo" width={30} />
                <p>Kuripara High School</p>
-               <p>কুড়িপাড়া উচ্চ বিদ্যালয়</p>
+               {/* <p>কুড়িপাড়া উচ্চ বিদ্যালয়</p> */}
             </div>
-            <div>
+            <div  className="flex flex-row items-center gap-4">
                <p>ABOUT</p>
                <p>NOTICES</p>
                <p>EVENTS</p>
