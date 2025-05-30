35e9659d3208df29b141faee48a82619194b0a35
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-05-30 19:24:49 +0600

    fix the nav

--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Logo from "@/assets/image/gov.png";
+import Dropdown_Nav from "../ui/Dropdown";
 
 
 export default function Navbar() {
    return(
    <div className=" sticky top-0 z-50">
+      {/* desktop */}
       <div className="bg-[#fff] shadow-2xl py-4 hidden md:flex md:flex-col  ">
          <div className="flex items-center justify-center gap-[200px] ">
             <div  className="flex flex-row items-center gap-3">
@@ -20,6 +22,15 @@ export default function Navbar() {
                <p className="text-xl font-bold ">LINKS</p>
             </div>
          </div>
+      </div>
+      {/* mobile */}
+      <div className="bg-[#fff] shadow-2xl py-2 flex md:hidden justify-between">
+         <div  className="flex flex-row items-center gap-3">
+            <Image src={Logo} alt="Logo" width={30} />
+            {/* <p className="text-xl font-bold  ">Kuripara High School</p> */}
+            <p className="text-xl font-bold  ">কুড়িপাড়া উচ্চ বিদ্যালয়</p>
+         </div>
+            <Dropdown_Nav/>
       </div>
    </div>
    );
