0d467777a09aa9cc897b548d693a93f5281656b4
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-06-02 22:49:17 +0600

    update

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,10 +2,36 @@
 import Image from "next/image";
 import Illustration from '@/assets/image/university-418219_1920.jpg'
 import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 
 export default function AdminAuth() {
-   const [isChecked, setIsChecked] = useState(false);
+   const router = useRouter();
+
+   const handelLogin = async(event:any) => {
+      event.preventDefault();
+      const name:string = event.target.name.value;
+      const password:string = event.target.password.value;
+      // console.log({email,password});
+      //  dispatch(setEmail(email));
+
+      // Get Api
+      const ApiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+      try {
+         const response = await fetch(`${ApiUrl}/auth`, {
+         method: 'POST',
+         headers: { 'Content-Type': 'application/json' },
+         body: JSON.stringify({ name, password })
+         });
+         const data = await response.json();
+         toast.success("Admin login is successful")
+         router.push("/admin");
+      } catch (error) {
+         toast.error("Account does not match")
+      }
+   }
    return(
       <div>
          <div className='relative bg-[#e7ebf0] '>
@@ -18,15 +44,15 @@ export default function AdminAuth() {
                      <h3 className='text-4xl font-bold'>Login As Admin</h3>
                      <p className='text-[#777272e3] font-bold'>Enter Admin Name and the a strong password</p>
                   </div>
-                  <form className="flex flex-col gap-[14px] ">
+                  <form onSubmit={handelLogin} className="flex flex-col gap-[14px] ">
                      {/* input name */}
                      <input type="text" name="name" id="name"  placeholder="Name"
                      className="border-[#E93B77] rounded-[8px] border-[1px] w-[90vw] sm:w-[300px] lg:w-[361px] h-[42px] p-4 "/>
                      {/* input password */}
-                     <input type="password" name="password" id="password" placeholder="Password" 
+                     <input type="text" name="password" id="password" placeholder="Password" 
                      className="border-[#E93B77] rounded-[8px] border-[1px] w-[90vw] sm:w-[300px] lg:w-[361px] h-[42px] p-4  "/>
                      {/* submit button */}
-                     <button className=" w-[90vw] sm:w-[300px] lg:w-[361px] h-[40px] bg-gradient-to-t from-[#E93B77] to-[#da6d93] rounded-[8px] ">
+                     <button type="submit" className=" w-[90vw] sm:w-[300px] lg:w-[361px] h-[40px] bg-gradient-to-t from-[#E93B77] to-[#da6d93] rounded-[8px] ">
                         Login
                      </button>
                   </form>
