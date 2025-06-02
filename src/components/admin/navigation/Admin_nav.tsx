274dcc3fdf0abffafb80faad2f30a824743c2f84
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-06-02 13:56:21 +0600

    customize ths navigation

--- src/components/admin/navigation/Admin_nav.tsx
+++ src/components/admin/navigation/Admin_nav.tsx
@@ -1,11 +1,31 @@
+import Image from "next/image";
+import Profile from "@/assets/image/ashraful.png";
+import { IoSettingsSharp } from "react-icons/io5";
+import { FaBell } from "react-icons/fa";
+
+
 
 
 export default function Admin_nav(){
     return(
         <div className="sticky top-4 z-50 bg-gradient-to-b from-[#ddc9c9] to-[#bbb9b9] w-[82.2vw] rounded-[12px] 
-        h-[62px] p-4 flex flex-row items-center justify-center ">
-            <div>
-                nav
+         h-[62px] p-0  ">
+            <div className="flex flex-row items-center justify-between mx-4 mt-[6px] ">
+                <div>
+                    <p></p>
+                    <p>Analytics</p>
+                </div>
+                <div className="flex flex-row items-center gap-5">
+                    <p className="text-2xl text-[#000000] ">
+                        <IoSettingsSharp/>
+                    </p>
+                    <p className="text-2xl text-[#000000] ">
+                       <FaBell/>
+                    </p>
+                    <div>
+                        <Image width={500} height={500} src={Profile} alt="Profile" className="rounded-full w-[50px] h-[50px] bg-white  " />
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/admin/navigation/Side_bar.tsx
+++ src/components/admin/navigation/Side_bar.tsx
@@ -1,28 +1,45 @@
 import Link from "next/link";
+import { FaHome } from "react-icons/fa";
+import { RiPagesFill } from "react-icons/ri";
+import { MdEvent } from "react-icons/md";
+import { MdEmojiEvents } from "react-icons/md";
+import { BsCalendar3EventFill } from "react-icons/bs";
+import { RiLockPasswordFill } from "react-icons/ri";
+
+
 
 
 export default function Side_bar(){
     return(
         <div className="sticky top-4 z-50 bg-gradient-to-t from-[#202020] to-[#3E3D45]  w-[250px]  h-[97vh] rounded-[12px]
         ml-2 p-4 text-white ">
-            <div >
-                <div>
-                    <Link href='/admin'>Home</Link>
+            <div className="flex flex-col gap-3">
+                <div className="flex flex-row items-center gap-6">
+                    <FaHome className="text-2xl "/>
+                    <Link href='/admin' className="hover:underline">Home</Link>
                 </div>
-                <div>
-                    <Link href='/admin/page/add-notice'>Add Notice</Link>
+                <hr className="w-[100%] text-white my-2 "/>
+                <div className="flex flex-row items-center gap-6">
+                    <RiPagesFill className="text-2xl "/>
+                    <Link href='/admin/page/add-notice' className="hover:underline">Add Notice</Link>
                 </div>
-                <div>
-                     <Link href='/admin/page/add-events'>Post Event</Link>
+                <div className="flex flex-row items-center gap-6">
+                    <MdEvent className="text-2xl "/>
+                     <Link href='/admin/page/add-events' className="hover:underline">Post Event</Link>
                 </div>
-                <div>
-                     <Link href='/admin/page/notice'>All Notice</Link>
+                <hr className="w-[100%] text-white  my-2 "/>
+                <div className="flex flex-row items-center gap-6">
+                    <BsCalendar3EventFill className="text-2xl "/>
+                     <Link href='/admin/page/notice' className="hover:underline">All Notice</Link>
                 </div>
-                <div>
-                     <Link href='/admin/page/event'>All Events</Link>
+                <div className="flex flex-row items-center gap-6">
+                    <MdEmojiEvents className="text-2xl "/>
+                     <Link href='/admin/page/event' className="hover:underline">All Events</Link>
                 </div>
-                <div>
-                    <Link href='/admin/page/chang-password'>Chang Password</Link>
+                <hr className="w-[100%] text-white my-2 "/>
+                <div className="flex flex-row items-center gap-6">
+                    <RiLockPasswordFill className="text-2xl "/>
+                    <Link href='/admin/page/chang-password' className="hover:underline">Chang Password</Link>
                 </div>
             </div>
         </div>
