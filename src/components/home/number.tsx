f2053b8cc8d4e08b291cfb4c1c53692322f7e3ba
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-06-04 19:43:20 +0600

    update

--- src/components/home/number.tsx
+++ src/components/home/number.tsx
@@ -8,33 +8,33 @@ import { FaHome } from "react-icons/fa";
 export default function Number() {
    return(
       <div className="flex justify-center mt-20 ">
-         <div className="flex flex-row items-center gap-[4.4vw] md:gap-[7vw] xl:gap-[10vw] bg-[#f8f4f4] shadow-2xl rounded-[8px] py-8 px-[6vw] sm:px-[8.3vw] ">
+         <div className="flex flex-row items-center justify-between w-[97vw] sm:w-[80vw] xl:w-[63.5vw] bg-[#f8f4f4] shadow-2xl rounded-[8px] py-8 px-[6vw] sm:px-[8.3vw] ">
             <div className="flex flex-col items-center gap-1">
                <div className="p-1 xl:p-7 rounded-full bg-[#6d676796] ">
                   <FaHome className="text-2xl  "/>
                </div>
-               <p className="text-xl font-bold"><CountUp end={100} duration={3} /></p>
+               <p className="text-xl font-bold"><CountUp end={1968} duration={3} /></p>
                <h3 className="text-md xl:text-xl font-bold">Founded</h3>
             </div>
             <div className="flex flex-col items-center gap-1">
                <div className="p-1 xl:p-7 rounded-full bg-[#6d676796] ">
                   <FaHome className=" text-2xl  "/>
                </div>
-               <p className="text-xl font-bold"><CountUp end={100} duration={3} /></p>
+               <p className="text-xl font-bold"><CountUp end={15} duration={3} /></p>
                <h3 className="text-md xl:text-xl font-bold">Teacher</h3>
             </div>
             <div className="flex flex-col items-center gap-1">
                <div className="p-1 xl:p-7 rounded-full bg-[#6d676796] ">
                   <FaHome className=" text-2xl  "/>
                </div>
-               <p className="text-xl font-bold"><CountUp end={100} duration={3} /></p>
+               <p className="text-xl font-bold"><CountUp end={1200} duration={3} /></p>
                <h3 className="text-md xl:text-xl font-bold">Student</h3>
             </div>
             <div className="flex flex-col items-center gap-1">
                <div className="p-1 xl:p-7 rounded-full bg-[#6d676796] ">
                   <FaHome className=" text-2xl  "/>
                </div>
-               <p className="text-xl font-bold"><CountUp end={100} duration={3} /></p>
+               <p className="text-xl font-bold"><CountUp end={5} duration={3} /></p>
                <h3 className="text-md xl:text-xl font-bold">Retting</h3>
             </div>
          </div>
