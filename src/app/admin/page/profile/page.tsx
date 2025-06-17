53a40ce2f0f4638f9d967a8e95e4a346498dfb40
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-06-17 11:15:01 +0600

    add profile

--- src/app/admin/page/profile/page.tsx
+++ src/app/admin/page/profile/page.tsx
@@ -1,4 +1,6 @@
 import PrivateRoute from "@/components/admin/auth/PrivateRoute";
+import Image from "next/image";
+import Profile from "@/assets/image/ashraful.png";
 
 
 
@@ -6,7 +8,17 @@ export default function AdminProfile(){
     return(
         <PrivateRoute>
             <div>
-                profile
+                <h2>Profile</h2>
+                <div>
+                    <div>
+                        <Image src={Profile} alt="profile" width={500} height={500} className="w-[300px] h-[300px] " />
+                    </div>
+                    <div>
+                        <p>Name  : <span className="text-sm font-semibold text-[#ffffffc9]">Ashraful</span></p>
+                        <p>Email : <span className="text-sm font-semibold text-[#ffffffc9]">ashrafulpathan3927@gmail.com</span></p>
+                        <p>Phone : <span className="text-sm font-semibold text-[#ffffffc9]">+8801614871378</span></p>
+                    </div>
+                </div>
             </div>
         </PrivateRoute>
     )
