d60d28ab59f450b08333ff4e5f32d439464c8867
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-30 21:41:30 +0600

    all text is add

--- src/components/home/official/ui/ConvertedSummaryTable.tsx
+++ src/components/home/official/ui/ConvertedSummaryTable.tsx
@@ -7,162 +7,123 @@ export default function ConvertedSummaryTable() {
     padding: "0in 0.08in",
     verticalAlign: "top",
   };
-  const sutonny: React.CSSProperties = { fontFamily: "SutonnyMJ, serif" };
 
   return (
-<div className="w-[100vw] md:w-full overflow-x-auto md:overflow-visible">
-    <div className="-mx-4 px-4 md:mx-0 md:px-0">
-      <table width={620} cellPadding={7} cellSpacing={0} style={tableStyle}>
-        <colgroup>
-          <col style={{ width: 110 }} />
-          <col style={{ width: 85 }} />
-          <col style={{ width: 88 }} />
-          <col style={{ width: 87 }} />
-          <col style={{ width: 74 }} />
-          <col style={{ width: 90 }} />
-        </colgroup>
+    <div className="w-[100vw] md:w-full overflow-x-auto md:overflow-visible">
+      <div className="-mx-4 px-4 md:mx-0 md:px-0">
+        <table width={620} cellPadding={7} cellSpacing={0} style={tableStyle}>
+          <colgroup>
+            <col style={{ width: 110 }} />
+            <col style={{ width: 85 }} />
+            <col style={{ width: 88 }} />
+            <col style={{ width: 87 }} />
+            <col style={{ width: 74 }} />
+            <col style={{ width: 90 }} />
+          </colgroup>
 
-        <tbody>
-          {/* Header */}
-          <tr style={{ verticalAlign: "top" }}>
-            <td style={{ ...cellStyle }} height={13}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>mb</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>
-                  †gvU cixÿv_x©
-                </span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>K…ZKvh©</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>AK…ZKvh©</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>wRwcG-5</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>cv‡ki nvi</span>
-              </p>
-            </td>
-          </tr>
+          <tbody>
+            {/* Header */}
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={{ ...cellStyle }} height={13}>
+                <p style={{ textAlign: "center" }}>
+                  <strong style={{ fontSize: 14 }}>সন</strong>
+                </p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center" }}>
+                  <strong style={{ fontSize: 14 }}>মোট পরীক্ষার্থী</strong>
+                </p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center" }}>
+                  <strong style={{ fontSize: 14 }}>কৃতকার্য</strong>
+                </p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center" }}>
+                  <strong style={{ fontSize: 14 }}>অকৃতকার্য</strong>
+                </p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center" }}>
+                  <strong style={{ fontSize: 14 }}>জিপিএ-৫</strong>
+                </p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center" }}>
+                  <strong style={{ fontSize: 14 }}>পাশের হার</strong>
+                </p>
+              </td>
+            </tr>
 
-          {/* Row: 2025 */}
-          <tr style={{ verticalAlign: "top" }}>
-            <td style={{ ...cellStyle }} height={39}>
-              <p>
-                <span style={{ ...sutonny, fontSize: 16 }}>Gm.Gm.wm-2025</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>119</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>88</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>31</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>02</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 14, fontWeight: 700 }}>73.95%</span>
-              </p>
-            </td>
-          </tr>
+            {/* Row: 2025 */}
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={{ ...cellStyle }} height={39}>
+                <p style={{ fontSize: 16 }}>এস.এস.সি-২০২৫</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center" }}><strong style={{ fontSize: 14 }}>119</strong></p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center" }}><strong style={{ fontSize: 14 }}>88</strong></p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center" }}><strong style={{ fontSize: 14 }}>31</strong></p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center" }}><strong style={{ fontSize: 14 }}>02</strong></p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center" }}><strong style={{ fontSize: 14 }}>73.95%</strong></p>
+              </td>
+            </tr>
 
-          {/* Row: 2024 */}
-          <tr style={{ verticalAlign: "top" }}>
-            <td style={{ ...cellStyle }} height={38}>
-              <p>
-                <span style={{ ...sutonny, fontSize: 16 }}>Gm.Gm.wm-2024</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 16 }}>139</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 16 }}>139</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 16 }}>0</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 16 }}>15</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 16 }}>100%</span>
-              </p>
-            </td>
-          </tr>
+            {/* Row: 2024 */}
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={{ ...cellStyle }} height={38}>
+                <p style={{ fontSize: 16 }}>এস.এস.সি-২০২৪</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>139</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>139</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>0</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>15</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>100%</p>
+              </td>
+            </tr>
 
-          {/* Row: 2023 */}
-          <tr style={{ verticalAlign: "top" }}>
-            <td style={{ ...cellStyle }} height={37}>
-              <p>
-                <span style={{ ...sutonny, fontSize: 16 }}>Gm.Gm.wm-2023</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 16 }}>145</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 16 }}>134</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 16 }}>11</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 16 }}>05</span>
-              </p>
-            </td>
-            <td style={cellStyle}>
-              <p style={{ textAlign: "center" }}>
-                <span style={{ ...sutonny, fontSize: 16 }}>92.41%</span>
-              </p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            {/* Row: 2023 */}
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={{ ...cellStyle }} height={37}>
+                <p style={{ fontSize: 16 }}>এস.এস.সি-২০২৩</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>145</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>134</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>11</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>05</p>
+              </td>
+              <td style={cellStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>92.41%</p>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-</div>
   );
 }
--- src/components/home/official/ui/TabilStudentNumber.tsx
+++ src/components/home/official/ui/TabilStudentNumber.tsx
@@ -9,290 +9,134 @@ export default function TabilStudentNumber() {
   };
 
   return (
-<div className="w-[100vw] md:w-full overflow-x-auto md:overflow-visible">
-    <div className="-mx-4 px-4 md:mx-0 md:px-0">
+    <div className="w-[100vw] md:w-full overflow-x-auto md:overflow-visible">
+      <div className="-mx-4 px-4 md:mx-0 md:px-0">
         <table width={623} cellPadding={7} cellSpacing={0} style={tableStyle}>
-        <colgroup>
+          <colgroup>
             <col style={{ width: 27 }} />
             <col style={{ width: 152 }} />
             <col style={{ width: 90 }} />
             <col style={{ width: 90 }} />
             <col style={{ width: 90 }} />
             <col style={{ width: 89 }} />
-        </colgroup>
+          </colgroup>
 
-        {/* Header row */}
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle}>
-            <p style={{ textAlign: "center" }}>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>µg</span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p style={{ textAlign: "center" }}>
-                <span style={{ color: "#1c2024" }}>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>kÖwY</span>
-                </span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p style={{ textAlign: "center" }}>
-                <span style={{ color: "#1c2024" }}>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>Q‡j</span>
-                </span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p style={{ textAlign: "center" }}>
-                <span style={{ color: "#1c2024" }}>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>g‡q</span>
-                </span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p style={{ textAlign: "center" }}>
-                <span style={{ color: "#1c2024" }}>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>gvU</span>
-                </span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p style={{ textAlign: "center" }}>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>gšÍe¨</span>
-            </p>
-            </td>
-        </tr>
+          <tbody>
+            {/* Header row */}
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>ক্রম</p>
+              </td>
+              <td style={thtdStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>শ্রেণি</p>
+              </td>
+              <td style={thtdStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>ছেলে</p>
+              </td>
+              <td style={thtdStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>মেয়ে</p>
+              </td>
+              <td style={thtdStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>মোট</p>
+              </td>
+              <td style={thtdStyle}>
+                <p style={{ textAlign: "center", fontSize: 16 }}>মন্তব্য</p>
+              </td>
+            </tr>
 
-        {/* Row 1 */}
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle}>
-            <p>1.</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>lô</span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p>95</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>100</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>195</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>&nbsp;</p>
-            </td>
-        </tr>
+            {/* Row 1 */}
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle}><p>1.</p></td>
+              <td style={thtdStyle}><p style={{ fontSize: 16 }}>ষষ্ঠ</p></td>
+              <td style={thtdStyle}><p>95</p></td>
+              <td style={thtdStyle}><p>100</p></td>
+              <td style={thtdStyle}><p>195</p></td>
+              <td style={thtdStyle}><p>&nbsp;</p></td>
+            </tr>
 
-        {/* Row 2 */}
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle}>
-            <p>2.</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>mßg</span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p>100</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>102</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>202</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>&nbsp;</p>
-            </td>
-        </tr>
+            {/* Row 2 */}
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle}><p>2.</p></td>
+              <td style={thtdStyle}><p style={{ fontSize: 16 }}>সপ্তম</p></td>
+              <td style={thtdStyle}><p>100</p></td>
+              <td style={thtdStyle}><p>102</p></td>
+              <td style={thtdStyle}><p>202</p></td>
+              <td style={thtdStyle}><p>&nbsp;</p></td>
+            </tr>
 
-        {/* Row 3 */}
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle}>
-            <p>3.</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>Aóg</span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p>85</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>100</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>185</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>&nbsp;</p>
-            </td>
-        </tr>
+            {/* Row 3 */}
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle}><p>3.</p></td>
+              <td style={thtdStyle}><p style={{ fontSize: 16 }}>অষ্টম</p></td>
+              <td style={thtdStyle}><p>85</p></td>
+              <td style={thtdStyle}><p>100</p></td>
+              <td style={thtdStyle}><p>185</p></td>
+              <td style={thtdStyle}><p>&nbsp;</p></td>
+            </tr>
 
-        {/* Rows 4.x (first cell has rowSpan=3) */}
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle} rowSpan={3}>
-            <p style={{ marginBottom: 0 }}>&nbsp;</p>
-            <p>4.</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>beg weÁvb</span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p>10</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>10</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>20</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>&nbsp;</p>
-            </td>
-        </tr>
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle}>
-            <p>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>
-                beg e¨emv wkÿv
-                </span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p>35</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>30</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>60</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>&nbsp;</p>
-            </td>
-        </tr>
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle}>
-            <p>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>
-                beg gvbweK
-                </span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p>35</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>45</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>80</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>&nbsp;</p>
-            </td>
-        </tr>
+            {/* Rows 4.x (first cell has rowSpan=3) */}
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle} rowSpan={3}>
+                <p style={{ marginBottom: 0 }}>&nbsp;</p>
+                <p>4.</p>
+              </td>
+              <td style={thtdStyle}><p style={{ fontSize: 16 }}>নবম বিজ্ঞান</p></td>
+              <td style={thtdStyle}><p>10</p></td>
+              <td style={thtdStyle}><p>10</p></td>
+              <td style={thtdStyle}><p>20</p></td>
+              <td style={thtdStyle}><p>&nbsp;</p></td>
+            </tr>
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle}><p style={{ fontSize: 16 }}>নবম ব্যবসা শিক্ষা</p></td>
+              <td style={thtdStyle}><p>35</p></td>
+              <td style={thtdStyle}><p>30</p></td>
+              <td style={thtdStyle}><p>60</p></td>
+              <td style={thtdStyle}><p>&nbsp;</p></td>
+            </tr>
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle}><p style={{ fontSize: 16 }}>নবম মানবিক</p></td>
+              <td style={thtdStyle}><p>35</p></td>
+              <td style={thtdStyle}><p>45</p></td>
+              <td style={thtdStyle}><p>80</p></td>
+              <td style={thtdStyle}><p>&nbsp;</p></td>
+            </tr>
 
-        {/* Rows 5.x (first cell has rowSpan=4) */}
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle} rowSpan={4}>
-            <p style={{ marginBottom: 0 }}>&nbsp;</p>
-            <p>5</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>
-                `kg weÁvb
-                </span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p>12</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>8</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>20</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>&nbsp;</p>
-            </td>
-        </tr>
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle}>
-            <p>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>
-                `kg e¨emv wkÿv
-                </span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p>45</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>35</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>70</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>&nbsp;</p>
-            </td>
-        </tr>
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle}>
-            <p>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>
-                `kg gvbweK
-                </span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p>50</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>35</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>85</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>&nbsp;</p>
-            </td>
-        </tr>
-        <tr style={{ verticalAlign: "top" }}>
-            <td style={thtdStyle}>
-            <p>
-                <span style={{ fontFamily: "SutonnyMJ, serif", fontSize: 16 }}>
-                me©‡gvU wkÿv_x©
-                </span>
-            </p>
-            </td>
-            <td style={thtdStyle}>
-            <p>467</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>463</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>930</p>
-            </td>
-            <td style={thtdStyle}>
-            <p>&nbsp;</p>
-            </td>
-        </tr>
+            {/* Rows 5.x (first cell has rowSpan=4) */}
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle} rowSpan={4}>
+                <p style={{ marginBottom: 0 }}>&nbsp;</p>
+                <p>5</p>
+              </td>
+              <td style={thtdStyle}><p style={{ fontSize: 16 }}>দশম বিজ্ঞান</p></td>
+              <td style={thtdStyle}><p>12</p></td>
+              <td style={thtdStyle}><p>8</p></td>
+              <td style={thtdStyle}><p>20</p></td>
+              <td style={thtdStyle}><p>&nbsp;</p></td>
+            </tr>
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle}><p style={{ fontSize: 16 }}>দশম ব্যবসা শিক্ষা</p></td>
+              <td style={thtdStyle}><p>45</p></td>
+              <td style={thtdStyle}><p>35</p></td>
+              <td style={thtdStyle}><p>70</p></td>
+              <td style={thtdStyle}><p>&nbsp;</p></td>
+            </tr>
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle}><p style={{ fontSize: 16 }}>দশম মানবিক</p></td>
+              <td style={thtdStyle}><p>50</p></td>
+              <td style={thtdStyle}><p>35</p></td>
+              <td style={thtdStyle}><p>85</p></td>
+              <td style={thtdStyle}><p>&nbsp;</p></td>
+            </tr>
+            <tr style={{ verticalAlign: "top" }}>
+              <td style={thtdStyle}><p style={{ fontSize: 16 }}>সর্বমোট শিক্ষার্থী</p></td>
+              <td style={thtdStyle}><p>467</p></td>
+              <td style={thtdStyle}><p>463</p></td>
+              <td style={thtdStyle}><p>930</p></td>
+              <td style={thtdStyle}><p>&nbsp;</p></td>
+            </tr>
+          </tbody>
         </table>
+      </div>
     </div>
-</div>
   );
 }
