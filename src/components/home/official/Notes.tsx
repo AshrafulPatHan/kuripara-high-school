e2de78a1d250d4d8c97d3bd59549dd6a97667f96
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-30 20:34:22 +0600

    add all data but the student data is stock

--- src/components/home/official/Notes.tsx
+++ src/components/home/official/Notes.tsx
@@ -2,6 +2,64 @@
 
 export default function NotesData(){
     return(
-        <div></div>
+        <div>
+            <div>
+                <h2>নোটিশ</h2>
+                <p className="main-text-intro">
+                    <span className="span-text-intro">
+                        এমপিও কোড:
+                    </span>
+                    ৩০০৩০৬১৩০১
+                </p>
+                <p className="main-text-intro">
+                    <span className="span-text-intro">
+                        ইআইএন নং:
+                    </span>
+                    ১১২৩৮৪
+                </p>
+                <p className="main-text-intro">
+                    <span className="span-text-intro">
+                        উপজেলা কোড:
+                    </span>
+                    ১৫২
+                </p>
+                <p className="main-text-intro">
+                    <span className="span-text-intro">
+                        জলা কোড:
+                    </span>
+                    ১৬
+                </p>
+                <p className="main-text-intro">
+                    <span className="span-text-intro">
+                        সেন্টার কোড:
+                    </span>
+                    ৫৮৭
+                </p>
+                <p className="main-text-intro">
+                    <span className="span-text-intro">
+                        প্রতিষ্ঠানের ফোন বা মোবাইল নম্বরসহ যোগাযোগের ঠিকানা:
+                    </span>
+                    ০১৮১৬১৮১৫৪১
+                </p>
+                <p className="main-text-intro">
+                    <span className="span-text-intro">
+                        তথ্য সেবা কেন্দ্রের ঠিকানা ও মোবাইল নম্বর:
+                    </span>
+                    কুড়িপাড়া, নাসিক-২৭ নং ওয়ার্ড। নম্বরঃ ০১৮১৬১৮১৫৪১
+                </p>
+                <p className="main-text-intro">
+                    <span className="span-text-intro">
+                        অভিযোগ নিষ্পত্তি কর্মকর্তার নাম ঠিকানা ও মোবাইল নম্বর:
+                    </span>
+                    ০১৭২০০২৩৬৮৮
+                </p>
+                <p className="main-text-intro">
+                    <span className="span-text-intro">
+                        যেকোনো তথ্য বা বিভিন্ন বিষয়াদি জানতে যোগাযোগ:
+                    </span>
+                    ০১৮১৩৪৮৫০৬১
+                </p>
+            </div>
+        </div>
     )
 }
--- src/components/home/official/Student.tsx
+++ src/components/home/official/Student.tsx
@@ -2,6 +2,11 @@
 
 export default function StudentsData(){
     return(
-        <div></div>
+        <div>
+            <div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
     )
 }
