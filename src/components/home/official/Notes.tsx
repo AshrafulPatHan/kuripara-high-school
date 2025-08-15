e7a2eab4f58904d903c13e9628bf879ba530ff00
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-15 15:02:58 +0600

    add the notes now responsive

--- src/components/home/official/Notes.tsx
+++ src/components/home/official/Notes.tsx
@@ -1,4 +1,3 @@
-// import { Phone, MapPin, Info } from "lucide-react";
 import { FaPhone } from "react-icons/fa6";
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { BsInfoSquareFill } from "react-icons/bs";
@@ -6,17 +5,19 @@ import { BsInfoSquareFill } from "react-icons/bs";
 
 
 export default function NotesData() {
-  const notes = [
-    { label: "এমপিও কোড", value: "৩০০৩০৬১৩০১" },
-    { label: "ইআইএন নং", value: "১১২৩৮৪" },
-    { label: "উপজেলা কোড", value: "১৫২" },
-    { label: "জেলা কোড", value: "১৬" },
-    { label: "সেন্টার কোড", value: "৫৮৭" },
-    { label: "প্রতিষ্ঠানের ফোন বা মোবাইল নম্বরসহ যোগাযোগের ঠিকানা", value: "০১৮১৬১৮১৫৪১", icon: <FaPhone className="w-4 h-4 text-blue-500" /> },
-    { label: "তথ্য সেবা কেন্দ্রের ঠিকানা ও মোবাইল নম্বর", value: "কুড়িপাড়া, নাসিক-২৭ নং ওয়ার্ড। নম্বরঃ ০১৮১৬১৮১৫৪১", icon: <FaMapMarkerAlt className="w-4 h-4 text-green-500" /> },
-    { label: "অভিযোগ নিষ্পত্তি কর্মকর্তার নাম ঠিকানা ও মোবাইল নম্বর", value: "০১৭২০০২৩৬৮৮", icon: <BsInfoSquareFill className="w-4 h-4 text-red-500" /> },
-    { label: "যেকোনো তথ্য বা বিভিন্ন বিষয়াদি জানতে যোগাযোগ", value: "০১৮১৩৪৮৫০৬১", icon: <FaPhone className="w-4 h-4 text-purple-500" /> },
-  ];
+    const notes = [
+        { label: "এমপিও কোড", value: "৩০০৩০৬১৩০১" },
+        { label: "ইআইএন নং", value: "১১২৩৮৪" },
+        { label: "উপজেলা কোড", value: "১৫২" },
+        { label: "জেলা কোড", value: "১৬" },
+        { label: "সেন্টার কোড", value: "৫৮৭" }
+    ];
+    const numbers = [
+        { label: "প্রতিষ্ঠানের ফোন বা মোবাইল নম্বরসহ যোগাযোগের ঠিকানা", value: "০১৮১৬১৮১৫৪১", icon: <FaPhone className="w-4 h-4 text-blue-500" /> },
+        { label: "তথ্য সেবা কেন্দ্রের ঠিকানা ও মোবাইল নম্বর", value: "কুড়িপাড়া, নাসিক-২৭ নং ওয়ার্ড। নম্বরঃ ০১৮১৬১৮১৫৪১", icon: <FaMapMarkerAlt className="w-4 h-4 text-green-500" /> },
+        { label: "অভিযোগ নিষ্পত্তি কর্মকর্তার নাম ঠিকানা ও মোবাইল নম্বর", value: "০১৭২০০২৩৬৮৮", icon: <BsInfoSquareFill className="w-4 h-4 text-red-500" /> },
+        { label: "যেকোনো তথ্য বা বিভিন্ন বিষয়াদি জানতে যোগাযোগ", value: "০১৮১৩৪৮৫০৬১", icon: <FaPhone className="w-4 h-4 text-purple-500" /> },
+    ];
 
   return (
     <section className="mt-20 px-4 mx-auto max-w-screen-lg">
@@ -24,16 +25,29 @@ export default function NotesData() {
         <h2 className="text-center mb-8 font-bold text-3xl text-gray-800">
           📒 নোটস
         </h2>
-        <div className="grid gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-5 mb-5">
           {notes.map((note, idx) => (
             <div
               key={idx}
               className="flex flex-col sm:flex-row items-start sm:items-center gap-2 p-4 bg-gray-50 hover:bg-gray-100 rounded-xl transition-all duration-200"
             >
-              {note.icon && <div className="flex-shrink-0">{note.icon}</div>}
               <div>
                 <span className="block font-semibold text-gray-700">{note.label}:</span>
                 <span className="text-gray-600">{note.value}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="grid gap-5">
+          {numbers.map((numbers, idx) => (
+            <div
+              key={idx}
+              className="flex flex-col sm:flex-row items-start sm:items-center gap-2 p-4 bg-gray-50 hover:bg-gray-100 rounded-xl transition-all duration-200"
+            >
+              {numbers.icon && <div className="flex-shrink-0">{numbers.icon}</div>}
+              <div>
+                <span className="block font-semibold text-gray-700">{numbers.label}:</span>
+                <span className="text-gray-600">{numbers.value}</span>
               </div>
             </div>
           ))}
